85dfb7b4f8f2d9e6109e4ac58fa57388dcab1652
BRUNOO1545 <brunogamer1545@gmail.com>
2024-07-22 12:20:19 -0300

    Mobile nav update

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let pageStarted = false;
+
+//#region startup
+
+function pageStartup() {
+    let pageStartupDiv = document.getElementById("pageStartupDiv");
+
+    pageStarted = true;
+    navHamburguerCollapse();
+    pageStartupDiv.classList = "page-startup page-startup-loaded";
+}
+
+//#endregion
+
+//#region Nav
+let navHamburguerOpen = false;
+
+// Check close
+function navHamburguerInteract() {
+    let navHamburguer = document.getElementById("navHamburguerCollapsable");
+    let navHamburguerIcon = document.getElementById("navHamburguerIcon");
+    
+    if (navHamburguerOpen === false) {
+        navHamburguerOpen = true;
+        navHamburguer.style.visibility = "hidden";
+        navHamburguer.style.height = 0;
+        navHamburguerIcon.src = "./assets/logo/page/hamburguer.svg";
+    } else {
+        navHamburguerOpen = false;
+        navHamburguer.style.visibility = "visible";
+        navHamburguer.style.height = "auto";
+        navHamburguerIcon.src = "./assets/logo/page/hamburguer_close.svg";
+    }
+}
+
+// Force close
+function navHamburguerCollapse() {
+    navHamburguerOpen = false;
+    navHamburguerInteract();
+}
+
+// Auto close nav
+let viewWidth = window.matchMedia("(max-width: 1080px)");
+
+viewWidth.addEventListener("change", function() {
+    navHamburguerCollapse();
+});
+
+//#endregion
